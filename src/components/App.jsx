a0395449f063c6c92da45a116e760938031896fb
Quarcoo-arc <53879894+Quarcoo-arc@users.noreply.github.com>
2021-06-14 16:27:37 +0000

    Introducing the form tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 function App() {
   let [name, setName] = useState("");
   let newName;
-  function getName() {
+  function getName(event) {
     setName(newName);
     console.log(newName);
+    event.preventDefault();
   }
   function createName(event) {
     newName = event.target.value;
@@ -14,13 +15,15 @@ function App() {
   return (
     <div className="container">
       <h1>Hello {name}</h1>
-      <input
-        type="text"
-        onChange={createName}
-        placeholder="What's your name?"
-        name="input"
-      />
-      <button onClick={getName}>Submit</button>
+      <form action="">
+        <input
+          type="text"
+          onChange={createName}
+          placeholder="What's your name?"
+          name="input"
+        />
+        <button onClick={getName}>Submit</button>
+      </form>
     </div>
   );
 }
